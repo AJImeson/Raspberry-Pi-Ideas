d639eaab4dc40f7aa05168e021ed3d17b01bbb66
axel.imeson@gmail.com <axel.imeson@gmail.com>
2025-09-18 15:31:34 +0200

    Raspberry7

--- RaspberryFunctions.py
+++ RaspberryFunctions.py
@@ -1,6 +1,5 @@
 
-## import paramiko
-import sys
+
 
 def Start(): # Booting script
     
@@ -9,14 +8,6 @@ def Start(): # Booting script
 
 Start()
 
-class Connect: # Remote Connect script
+
     
-   def __init__(self, host, port, user, key_path, remote_command, local_file, remote_path):
-       self.host = host
-       self.port = port
-       self.user = user
-       self.key_path = key_path
-       self.remote_command = remote_command
-       self.local_file = local_file
-       self.remote_path = remote_path
-    
+        
